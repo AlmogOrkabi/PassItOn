2cf257bca4c5567a93370a37ba87a47c3b759879
AlmogOrkabi <117260151+AlmogOrkabi@users.noreply.github.com>
2023-05-30 16:14:48 +0300

    .

---
+++ PassItOn/scr/Screens/Start.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView, Image, Button } from 'react-native'
+import React from 'react'
+
+export default function Start() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>ברוכים הבאים</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
